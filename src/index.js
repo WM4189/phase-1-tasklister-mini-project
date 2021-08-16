a5d4c5f3de2b1fab94c8edc4c3295a70e9df147e
William Mosto <will.mosto@gmail.com>
2021-08-16 19:39:19 -0400

    Select DropDown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  let select = document.querySelector('select')
+
+  select.addEventListener('change',changeDay) 
+ 
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+
+function changeDay() {
+  let select = document.querySelector('select')
+  let h1 = document.getElementsByTagName('h1')
+  let h2 = document.getElementsByTagName('h2')
+  h1[0].textContent = `Will's ${select.value}`
+  h2[0].textContent = `${select.value} List`
+}
+
+
+
+function buildToDo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = "x"
+  li.textContent = `${todo}    `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+} 
+
+
+
+
